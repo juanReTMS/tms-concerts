cbe14261ca7fcff8edb0546bc8039c7ec4fbd60a
alex1701c <38406607+alex1701c@users.noreply.github.com>
2019-04-05 19:29:54 +0200

    Update CreateEntitiesTest.php

--- tests/CreateEntitiesTest.php
+++ tests/CreateEntitiesTest.php
@@ -15,36 +15,29 @@ class CreateEntitiesTest extends WebTestCase
      */
     private $em;
 
-    /**
-     * @var ManagerRegistry
-     */
-    private $doctrine;
-
 
     public function __construct($name = null, array $data = [], string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $client = static::createClient();
         $this->em = $client->getContainer()->get('doctrine')->getManager();
-        $this->doctrine = $client->getContainer()->get('doctrine');
         $this->em->beginTransaction();
     }
 
     public function testLocation()
     {
         $location = new Location();
-        $location->setInstitution("TMS Bad Oldesloe");
+        $location->setInstitution("TMS");
         $location->setBuilding("K");
         $location->setFloor("Erdgeschoss");
         $location->setRoom("K042");
-        $location->setDescription("Description ...");
+        $location->setDescription("...");
         $location->setSeats(42);
 
         self::assertNull($location->getId());
 
         $this->em->persist($location);
         $this->em->flush();
-        echo $location->getId();
+        
         self::assertNotNull($location->getId());
 
     }
