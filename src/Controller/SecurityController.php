a80515f5b46fe55bcf345c1bde04900a70c3b2bc
alex1701c <alexkp12355@gmail.com>
2019-05-27 19:31:38 +0200

    Register added

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Person;
+use App\Form\PersonRegisterType;
+use App\Form\PersonType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -17,6 +21,28 @@ class SecurityController extends AbstractController
         $error = $utils->getLastAuthenticationError();
         return $this->render('security/login.html.twig',
             ['error' => $error, 'last_email' => $utils->getLastUsername()]);
+
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $person = new Person();
+        $form = $this->createForm(PersonRegisterType::class, $person);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $person->setPassword($encoder->encodePassword($person, $person->getPlainPassword()));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+            $em->flush();
+            return $this->redirectToRoute('login');
+        }
+
+        return $this->render('security/register.html.twig',
+            ['form' => $form->createView()]);
 
     }
 
---
+++ src/Form/PersonRegisterType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonRegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('telephone')
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
